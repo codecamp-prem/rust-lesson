b422612452b646d91bac5806f0f936577c6373c6
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-08-16 15:33:02 +0545

    created so all rust lesson will be in src/bin folder

---
+++ src/bin/match_expression.rs
@@ -0,0 +1,8 @@
+fn main(){
+    let name = "James";
+    match name{
+        "John" => println!("Name is John Cena"),
+        "James" => println!("James, James Bond"), 
+        _ => println!("Hello there General Kneobi"),
+    }
+}
