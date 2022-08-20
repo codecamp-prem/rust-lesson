b2390750caeb93f1eb8753acdf855694027dc0c8
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-08-20 09:24:36 +0545

    vector in Rust demo 1

---
+++ src/bin/vector_demo1.rs
@@ -0,0 +1,11 @@
+fn main(){
+    let my_numbers = vec![10, 20, 30, 40];
+
+    for num in &my_numbers {
+        match num {
+            30 => println!("thirty"),
+            _ => println!("{:?}", num),
+        }
+    }
+    println!("Length of Vector: {:?}", my_numbers.len());
+}
