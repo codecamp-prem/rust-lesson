7872112e6514a2663bc21cab331bf5ed129866d7
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-08-23 13:29:42 +0545

    generating document in Rust

---
+++ src/bin/generating_doc.rs
@@ -0,0 +1,18 @@
+/// Color availiable for use
+enum Color{
+    Red,
+    Blue,
+}
+
+/// Mail struct 
+struct Mail{
+    /// destination address
+    address: String,
+}
+
+ /// Adds two numbers
+ fn add(a: i32, b: i32) -> i32 {
+    a + b
+ }
+
+ fn main(){}
