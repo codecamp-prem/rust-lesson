db2b0eeb1608e42220e2d6a32d9f930107f52e90
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-09-02 15:37:47 +0545

    The Default Trait demo program

---
+++ src/bin/default_trait_demo.rs
@@ -0,0 +1,23 @@
+struct Package{
+    weight: f64,
+}
+
+impl Package{
+    fn new(wight: f64)->Self{
+        Self{
+            weight
+        }
+    }
+}
+
+impl Default for Package{
+    fn default() -> Self{
+        Self{
+            weight: 3.0
+        }
+    }
+}
+
+fn main(){
+    let p = Package::default();
+}
