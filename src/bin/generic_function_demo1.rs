e9f37bbcc734fa8d6bae38647b39e2314e87990a
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-09-03 08:32:16 +0545

    Generic function demo program with enum and struct

---
+++ src/bin/generic_function_demo1.rs
@@ -0,0 +1,46 @@
+// Topic: Generics & Functions
+//
+// Requirements:
+// * Create a function that accepts the Priority trait as a generic parameter
+//   * The function should print out the guest and their priority
+// * Use the function with at least two different guests
+//
+// Notes:
+// * Use the debug token "{:?}" to print out the information
+// * Use the compiler to guide you to the correct generic constraints needed
+
+#[derive(Debug)]
+enum ServicePriority {
+    High,
+    Standard,
+}
+
+trait Priority {
+    fn get_priority(&self) -> ServicePriority;
+}
+
+#[derive(Debug)]
+struct ImportantGuest;
+impl Priority for ImportantGuest {
+    fn get_priority(&self) -> ServicePriority {
+        ServicePriority::High
+    }
+}
+
+#[derive(Debug)]
+struct Guest;
+impl Priority for Guest {
+    fn get_priority(&self) -> ServicePriority {
+        ServicePriority::Standard
+    }
+}
+
+fn print_priority<T: Priority + std::fmt::Debug>(guest: T){
+    let priority = guest.get_priority();
+    println!("{:?} is {:?} priority", guest, priority);
+}
+
+fn main() {
+    print_priority(Guest{}); // Guest is Standard priority
+    print_priority(ImportantGuest{}); // ImportantGuest is High priority
+}
