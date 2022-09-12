2a334fdd2af96452f8251d6a951b2adbc762b04f
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-09-12 20:39:38 +0545

    Match Guards Binding demo 2- match Struct

--- src/bin/match_guards_binding1.rs
+++ src/bin/match_guards_binding1.rs
@@ -14,11 +14,11 @@ fn main(){
         age: 17,
         species: Species::Hawk,
     };
-
+    //Matching Structure
     match hawk{
-        Bird{ age: 4, .. } => println!("4 years old bird"),
+        Bird{ age: 4, .. } => println!("4 years old bird"), // .. ignore everthing else in this context
         Bird{ age: 4..=10 | 15..=20, .. } => println!("4-10 or 15-20 year old bird"),
         Bird{ species: Species::Parrot, .. } => println!("Parrot!"),
-        Bird{ .. } => println!("Other Bird")
+        Bird{ .. } => println!("Other Bird") //.. indicates everthing else in this context
     }
 }
