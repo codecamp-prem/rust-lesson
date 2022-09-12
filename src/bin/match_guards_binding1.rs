e2316169b6b6ca5e1e6f849a4e84b6eed77047a8
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-09-12 20:36:24 +0545

    Match Guards Binding demo 2

---
+++ src/bin/match_guards_binding1.rs
@@ -0,0 +1,24 @@
+enum Species {
+    Finch,
+    Hawk,
+    Parrot,
+}
+
+struct Bird{
+    age: usize,
+    species: Species,
+}
+
+fn main(){
+    let hawk = Bird{
+        age: 17,
+        species: Species::Hawk,
+    };
+
+    match hawk{
+        Bird{ age: 4, .. } => println!("4 years old bird"),
+        Bird{ age: 4..=10 | 15..=20, .. } => println!("4-10 or 15-20 year old bird"),
+        Bird{ species: Species::Parrot, .. } => println!("Parrot!"),
+        Bird{ .. } => println!("Other Bird")
+    }
+}
