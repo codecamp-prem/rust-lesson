64b7bdde88e33816029e3ec815218445b37ef953
codecamp-prem <prem.bikram.limbu@gmail.com>
2022-11-11 07:34:49 +0545

    Run the provided functions in threads and retireve the data from the threads to print the message.

---
+++ src/bin/threads_demo4.rs
@@ -0,0 +1,40 @@
+// Topic: Multithreading
+//
+// Requirements:
+// * Run the provided functions in threads
+// * Retrieve the data from the threads to print the message
+//   "Hello, threads!"
+//
+// Notes:
+// * Use the join function to wait for threads to finish
+
+fn msg_hello() -> &'static str {
+    use std::time::Duration;
+    std::thread::sleep(Duration::from_millis(1000));
+    "Hello, "
+}
+
+fn msg_thread() -> &'static str {
+    use std::time::Duration;
+    std::thread::sleep(Duration::from_millis(1000));
+    "threads"
+}
+
+fn msg_excited() -> &'static str {
+    use std::time::Duration;
+    std::thread::sleep(Duration::from_millis(1000));
+    "!"
+}
+
+fn main() {
+    use std::thread;
+    let msg_1 = thread::spawn(move || msg_hello());
+    let msg_2 = thread::spawn(move || msg_thread());
+    let msg_3 = thread::spawn(move || msg_excited());
+
+    let msg_1 = msg_1.join().expect("failed to join msg 1");
+    let msg_2 = msg_2.join().expect("failed to join msg 2");
+    let msg_3 = msg_3.join().expect("failed to join msg 3");
+
+    println!("{}{}{}", msg_1, msg_2, msg_3);
+}
